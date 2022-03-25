648292ac4a21ad69a4e7216cbbc9dbad9b1ad7a9
Matei Mandache <matei.mandache@gmail.com>
2022-03-25 18:17:53 +0000

    map completed

--- map.py
+++ map.py
@@ -1,15 +1,29 @@
 from graphics import *
 from math import sqrt
 from enum import Enum
+from random import uniform, gauss
 
 
 BACKGROUND_COLOUR = "black"
 ORIGIN_X = 400
 ORIGIN_Y = 300
-STEP = 100
+STEP = 70
 WIDTH = 800
 HEIGHT = 600
 DETECTION_FACTOR = 0.5
+X_MAX = (WIDTH - ORIGIN_X) / STEP
+X_MIN = - ORIGIN_X / STEP
+Y_MAX = ORIGIN_Y / STEP
+Y_MIN = (ORIGIN_Y - HEIGHT) / STEP
+X_LIMIT = max([X_MAX, -X_MIN])
+Y_LIMIT = max([Y_MAX, -Y_MIN])
+PRECISION = 0.000_01
+JUMP = 0.1
+NUM_CONTOURS = 50
+DEGREE = 6
+COLOUR_CONSTANT = 256
+COLOUR_MAX = 255
+
 
 class JumpType(Enum):
     LEFT = "LEFT"
@@ -23,12 +37,14 @@ class Screen(GraphWin):
         self.origin_y = origin_y
         self.step = step
 
-    def draw_line(self, start_x, start_y, end_x, end_y):
+    def draw_line(self, start_x, start_y, end_x, end_y, colour):
         start = Point(int(self.step * start_x) + self.origin_x,
                       -int(self.step * start_y) + self.origin_y)
         end = Point(int(self.step * end_x) + self.origin_x,
                     -int(self.step * end_y) + self.origin_y)
-        Line(start, end).draw(self)
+        line = Line(start, end)
+        line.setOutline(colour)
+        line.draw(self)
 
 
 class Polynomial:
@@ -61,6 +77,20 @@ class Polynomial:
     def __str__(self):
         return str(self.coefficients)
 
+    @classmethod
+    def random(cls, degree):
+        coefficients = []
+        multiplier = 1
+        for i in range(degree + 1):
+            row = []
+            full_multiplier = multiplier
+            for j in range(degree + 1 - i):
+                row.append(gauss(0, full_multiplier))
+                full_multiplier /= (j+1)
+            coefficients.append(row)
+            multiplier /= (i+1)
+        return cls(coefficients)
+
 
 class Poly(Polynomial):
     def __init__(self, coefficients):
@@ -82,7 +112,8 @@ class Poly(Polynomial):
             start_y = new_y
         return new_x, new_y
 
-    def jump(self, surface, start_x, start_y, jump_distance, jump_type, precision):
+    def jump(self, surface, start_x, start_y, jump_distance,
+             jump_type, precision, colour):
         value = self.apply(start_x, start_y)
         ddx = self.ddx.apply(start_x, start_y)
         ddy = self.ddy.apply(start_x, start_y)
@@ -96,10 +127,11 @@ class Poly(Polynomial):
             new_x, new_y = self.newton_raphson(start_x + ddy * multiplier,
                                                start_y - ddx * multiplier,
                                                value, precision)
-        surface.draw_line(start_x, start_y, new_x, new_y)
+        surface.draw_line(start_x, start_y, new_x, new_y, colour)
         return new_x, new_y
 
-    def plot_contour(self, surface, x, y, jump_distance, precision, x_limit, y_limit):
+    def plot_contour(self, surface, x, y, jump_distance, precision,
+                     x_limit, y_limit, colour):
         x_left, y_left = x, y
         x_right, y_right = x, y
         first_time = True
@@ -107,8 +139,8 @@ class Poly(Polynomial):
         right_in_bounds = True
         while True:
             if left_in_bounds:
-                x_left, y_left = self.jump(surface, x_left, y_left,
-                                           jump_distance, JumpType.LEFT, precision)
+                x_left, y_left = self.jump(surface, x_left, y_left, jump_distance,
+                                           JumpType.LEFT, precision, colour)
                 if abs(x_left) > x_limit or abs(y_left) > y_limit:
                     if not right_in_bounds:
                         break
@@ -116,11 +148,11 @@ class Poly(Polynomial):
                 if not first_time:
                     if ((x_left - x_right) ** 2 + (y_left - y_right) ** 2 <
                             jump_distance ** 2 * DETECTION_FACTOR):
-                        surface.draw_line(x_left, y_left, x_right, y_right)
+                        surface.draw_line(x_left, y_left, x_right, y_right, colour)
                         break
             if right_in_bounds:
-                x_right, y_right = self.jump(surface, x_right, y_right,
-                                             jump_distance, JumpType.RIGHT, precision)
+                x_right, y_right = self.jump(surface, x_right, y_right, jump_distance,
+                                             JumpType.RIGHT, precision, colour)
                 if abs(x_right) > x_limit or abs(y_right) > y_limit:
                     if not left_in_bounds:
                         break
@@ -128,7 +160,51 @@ class Poly(Polynomial):
                 if not first_time:
                     if ((x_left - x_right) ** 2 + (y_left - y_right) ** 2 <
                             jump_distance ** 2 * DETECTION_FACTOR):
-                        surface.draw_line(x_left, y_left, x_right, y_right)
-                        print(x_left, y_left, x_right, y_right)
+                        surface.draw_line(x_left, y_left, x_right, y_right, colour)
                         break
             first_time = False
+
+
+def rainbow(index):
+    index *= 6
+    if index < 1:
+        return color_rgb(int(COLOUR_CONSTANT*index), 0, 0)
+    elif index < 2:
+        index %= 1
+        return color_rgb(COLOUR_MAX, int(COLOUR_CONSTANT*index), 0)
+    elif index < 3:
+        index %= 1
+        return color_rgb(COLOUR_MAX-int(COLOUR_CONSTANT*index), COLOUR_MAX, 0)
+    elif index < 4:
+        index %= 1
+        return color_rgb(0, COLOUR_MAX, int(COLOUR_CONSTANT*index))
+    elif index < 5:
+        index %= 1
+        return color_rgb(0, COLOUR_MAX-int(COLOUR_CONSTANT*index), COLOUR_MAX)
+    else:
+        index %= 1
+        return color_rgb(int(COLOUR_CONSTANT*index), 0, COLOUR_MAX)
+
+
+def main():
+    screen = Screen(ORIGIN_X, ORIGIN_Y, STEP, width=WIDTH, height=HEIGHT)
+    screen.setBackground("black")
+    points = []
+    for _ in range(NUM_CONTOURS):
+        points.append((uniform(X_MIN, X_MAX), uniform(Y_MIN, Y_MAX)))
+    poly = Poly.random(DEGREE)
+    values = []
+    for i, point in enumerate(points):
+        values.append((poly.apply(point[0], point[1]), i))
+    values.sort()
+    colour_indices = [None] * NUM_CONTOURS
+    for i, value in enumerate(values):
+        colour_indices[value[1]] = i / NUM_CONTOURS
+    for point, colour_index in zip(points, colour_indices):
+        poly.plot_contour(screen, point[0], point[1], JUMP, PRECISION,
+                          X_LIMIT, Y_LIMIT, rainbow(colour_index))
+    screen.getMouse()
+
+
+if __name__ == "__main__":
+    main()
--- map_test.py
+++ map_test.py
@@ -12,11 +12,12 @@ def test():
     print(test_poly.y_derivative())
     print(test_poly.apply(*test_poly.newton_raphson(1.12, 3.97, 1, 0.001)))
     screen = Screen(ORIGIN_X, ORIGIN_Y, STEP, width=WIDTH, height=HEIGHT)
-    test_poly.plot_contour(screen, -0.3, -2.1, 0.1, 0.001, 4, 3)
-    test_poly.plot_contour(screen, -0.3, -1.1, 0.1, 0.001, 4, 3)
-    test_poly.plot_contour(screen, -0.3, -0.1, 0.1, 0.001, 4, 3)
-    test_poly.plot_contour(screen, -0.3, -2.6, 0.1, 0.001, 4, 3)
-    test_poly.plot_contour(screen, 2.3, -0.6, 0.1, 0.001, 4, 3)
+    test_poly.plot_contour(screen, -0.3, -2.1, 0.1, 0.001, 4, 3, "red")
+    test_poly.plot_contour(screen, -0.3, -1.1, 0.1, 0.001, 4, 3, "black")
+    test_poly.plot_contour(screen, -0.3, -0.1, 0.1, 0.001, 4, 3, "blue")
+    test_poly.plot_contour(screen, -0.3, -2.6, 0.1, 0.001, 4, 3, "green")
+    test_poly.plot_contour(screen, 2.3, -0.6, 0.1, 0.001, 4, 3, "yellow")
+    print(Polynomial.random(5))
     screen.getMouse()
 
 
